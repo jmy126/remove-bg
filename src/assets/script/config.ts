5148817f410e5d4382a54da3b2006e5a6ee902a8
daxmeng <dajunjn@126.com>
2025-08-19 17:08:22 +0800

    chore: 更新onnxruntime-web

--- src/assets/script/config.ts
+++ src/assets/script/config.ts
@@ -4,7 +4,7 @@ export const MODEL_URL =
   'https://huggingface.co/djwild/remove-bg/resolve/main/u2netp.onnx';
 
 export const WASM_PATH =
-  'https://cdn.jsdelivr.net/npm/onnxruntime-web@1.18.0/dist/';
+  'https://cdn.jsdelivr.net/npm/onnxruntime-web@1.22.0/dist/';
 
 // export const MODEL_URL = 'http://127.0.0.1:8080/u2netp.onnx';
 // export const WASM_PATH = 'http://127.0.0.1:3000/';
